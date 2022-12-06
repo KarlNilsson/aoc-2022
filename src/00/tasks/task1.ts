e205ec089a8c4ebdb1f455e69e8c99b8730d6f57
Karl Nilsson <meisterkarl@gmail.com>
2022-12-06 12:26:22 +0100

    Remove console log from example task files

--- src/00/tasks/task1.ts
+++ src/00/tasks/task1.ts
@@ -4,7 +4,6 @@ const Task = async () => {
   const data = await readFile(`${__dirname}/../../../input/00/input1.txt`, {
     encoding: 'utf8'
   });
-  console.log(data);
 };
 
 export default Task;
